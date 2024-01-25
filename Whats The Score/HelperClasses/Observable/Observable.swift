222d44617f4b5481f0855cb9c0d3254ee5822d0f
Curt McCune <curtmccune@gmail.com>
2024-01-25 08:58:31 -0700

    Added and tested an Observable class

---
+++ Whats The Score/HelperClasses/Observable/Observable.swift
@@ -0,0 +1,33 @@
+//
+//  Observable.swift
+//  Whats The Score
+//
+//  Created by Curt McCune on 1/25/24.
+//
+
+import Foundation
+
+class Observable<ObservedType> {
+    private var _value: ObservedType?
+    
+    var valueChanged: ((ObservedType?) -> Void)?
+    
+    public var value: ObservedType? {
+        get {
+            return _value
+        }
+        
+        set {
+            _value = newValue
+            valueChanged?(_value)
+        }
+    }
+
+    init(_ value: ObservedType) {
+        _value = value
+    }
+    
+//    func bindingChanged(to newValue: ObservedType) {
+//        _value = newValue
+//    }
+}
---
+++ Whats The Score/HelperClasses/Observable/ObservableTests.swift
@@ -0,0 +1,42 @@
+//
+//  ObservableTests.swift
+//  Whats The Score Tests
+//
+//  Created by Curt McCune on 1/25/24.
+//
+
+import XCTest
+@testable import Whats_The_Score
+
+final class ObservableTests: XCTestCase {
+
+    func test_Observable_WhenValueChanged_ShouldSetNewValue() {
+        // given
+        let sut = Observable(0)
+        var newValue = Int.random(in: 1...10)
+        
+        // when
+        sut.value = newValue
+        
+        // then
+        XCTAssertEqual(sut.value, newValue)
+    }
+
+    func test_Observable_WhenValueChanged_ShouldCallValueChangedFunction() {
+        // given
+        let sut = Observable(0)
+        var newValue = Int.random(in: 1...10)
+        let expectation = XCTestExpectation(description: "Value changed should be called")
+        
+        sut.valueChanged = { value in
+            expectation.fulfill()
+            XCTAssertEqual(value, newValue)
+        }
+        
+        // when
+        sut.value = newValue
+        
+        // then
+        wait(for: [expectation])
+    }
+}
