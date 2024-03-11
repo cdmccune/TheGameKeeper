6217e58b678112c58f613246f2e99ff041ec2a1e
Curt McCune <curtmccune@gmail.com>
2024-03-11 14:57:03 -0600

    Card #21 Completed - Put a plus/minus button on the toolbar for keyboard in the EndRoundPopoverVC

---
+++ Whats The Score/Scoreboard/EndRoundPopover/TextField/EndRoundPopoverTextField.swift
@@ -0,0 +1,20 @@
+//
+//  EndRoundPopoverTextField.swift
+//  Whats The Score
+//
+//  Created by Curt McCune on 3/11/24.
+//
+
+import UIKit
+
+class EndRoundPopoverTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
