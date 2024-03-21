e9d1c1f48989751b70ebea04324a56f8603105c1
Curt McCune <curtmccune@gmail.com>
2024-03-21 12:40:22 -0600

    test commit

--- Whats The Score/GameTabCoordinator/GameSetupCoordinator/GameSetupCoordinator.swift
+++ Whats The Score/GameTabCoordinator/GameSetupCoordinator/GameSetupCoordinator.swift
@@ -55,5 +55,5 @@ class GameSetupCoordinator: Coordinator {
         let playerSetupVC = PlayerSetupViewController.instantiate()
         navigationController.pushViewController(playerSetupVC, animated: true)
     }
+    print("test change")
  }
-print("test change")
